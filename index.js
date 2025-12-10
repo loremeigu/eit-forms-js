c4bc756c1a547e05bd607b59096ac1b92634419d
loremeigu <lore.meigu@gmail.com>
2025-12-09 21:58:58 -0300

    ADD Js examples

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Hello world!")
+
+var a = "hola"
+console.log(a)
+a = "chau"
+console.log(a)
+if(true){
+    console.log(a)
+}
+
+let b
+console.log(b)
+b=55
+console.log(b)
+if(true){
+    console.log(b)
+}
+
+const c = 99
+console.log(c)
+console.log(c.toString())
